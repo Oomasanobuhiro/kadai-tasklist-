42b171da673d291fd7139a568efc043fc7426125
nobuhiro.oomasa <oomasa.nobuhiro@trans-cosmos.co.jp>
2025-06-11 11:16:41 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TasksController;
+
+/*
+（コメント部分は省略）
+*/
+
+Route::get('/', [TasksController::class, 'index']);
+Route::resource('tasks', TasksController::class);
